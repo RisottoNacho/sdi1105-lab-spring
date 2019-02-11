dd601dc702c1622630f75faeaa0908f538db9d1d
uo258014 <ignacieiro@gmail.com>
2019-02-11 14:03:00 +0100

    SDI - 1.11 Vistas – Motor de plantillas thymelea

---
+++ src/main/java/com/uniovi/Sdi1105LabSpringApplication.java
@@ -0,0 +1,15 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1105LabSpringApplication {
+
+	public static void main(String[] args) {
+		//tytt
+		SpringApplication.run(Sdi1105LabSpringApplication.class, args);
+	}
+
+}
+
